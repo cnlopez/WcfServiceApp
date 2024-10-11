c685140f24daa6c0a867f318eb6e803b21bfd921
nikolai <cnlopez1@hotmail.com>
2024-10-10 19:44:20 -0500

    Add project files.

---
+++ ClientCallService/ClientCallService/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+string result = await client.GetDataAsync(5);
+Console.WriteLine(result);
+client.Close();
